efb2b2ae21267e0c1cf0b622532063bd50f796e2
rakibul <rakibul.cse.47@gmail.com>
2024-03-09 17:11:32 +0600

    safe commit- office09Mar2024

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = "http://202.22.203.92:3100";
-//const URL = 'http://192.168.61.40:3000';
 
 export const socket = io.connect(URL, {
   transports: ["websocket"],
