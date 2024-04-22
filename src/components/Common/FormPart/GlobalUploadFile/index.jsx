bb152c4ba6e7f6b4112a6fe2700e96659854d12b
FullstackRakibul <rakibul.979.hasan@gmail.com>
2024-04-22 16:58:43 +0600

    file upload --22office

--- src/components/Common/FormPart/GlobalUploadFile/index.jsx
+++ src/components/Common/FormPart/GlobalUploadFile/index.jsx
@@ -5,20 +5,64 @@ import { PlusOutlined } from "@ant-design/icons";
 
 const GlobalUploadFile = () => {
   const [fileUpload, setFileUpload] = useState([]);
+  const [fileList, setFileList] = useState([]);
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+
+  const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
+  
   const [form] = Form.useForm();
+
+
+   const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+  };
+
 
   const handleFileUpload = async (values) => {
     try {
+
       const formData = new FormData();
-      formData.append("fileUpload", values.fileUpload);
+      formData.append("UploadedFile", values.Uploadfile.file);
+
+    //   fileList.forEach((file) => {
+    //   formData.append("UploadedFile", values.Uploadfile.file.originFileObj);
+    // });
+
+      // hardCoderData
+      formData.append("TicketId","10");
+      formData.append("FolderIndex","1");
+      formData.append("FilePathUrl","GloballlyUploadedFile");
+
+
+      console.log(formData);
+
+      // const UploadedData = {
+      //   UploadedFile:values.file.originFileObj,
+      //   TicketId:10,
+      //   FolderIndex:1,
+      //   FilePathUrl:"GloballlyUploadedFile"
+      // }
+      // console,log(UploadedData);
 
       const response = await AxiosInstance.post("/api/FileUpload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-
       console.log(response);
+
       message.success("File Upload Success!");
       //form.resetFields();
     } catch (error) {
@@ -28,35 +72,10 @@ const GlobalUploadFile = () => {
   };
   return (
     <>
-      {/* <Form onFinish={handleFileUpload} form={form}>
-        <Form.Item label="Upload" name="fileUpload">
-          <Upload listType="picture-card" beforeUpload={() => false}>
-            <button
-              style={{
-                border: 0,
-                background: "none",
-              }}
-              type="button"
-            >
-              <PlusOutlined />
-              <div
-                style={{
-                  marginTop: 8,
-                }}
-              >
-                Upload
-              </div>
-            </button>
-          </Upload>
-        </Form.Item>
-        <Form.Item label="Button">
-          <Button htmlType="submit">Button</Button>
-        </Form.Item>
-      </Form> */}
-
       <Form onFinish={handleFileUpload} form={form}>
-        <Form.Item label="Upload" name="filePathUrl">
+        <Form.Item label="Upload" name="Uploadfile">
           <Upload
+            //multiple={true}
             beforeUpload={() => false}
             listType="picture-card"
             onChange={(info) => {
@@ -65,12 +84,27 @@ const GlobalUploadFile = () => {
             onRemove={() => {
               setFileUpload([]);
             }}
+            onPreview={handlePreview}
           >
             <button style={{ border: 0, background: "none" }} type="button">
               <PlusOutlined />
               <div style={{ marginTop: 8 }}>Upload</div>
             </button>
           </Upload>
+           {previewImage && (
+          <Image
+            wrapperStyle={{
+              display: 'none',
+            }}
+            preview={{
+              visible: previewOpen,
+              onVisibleChange: (visible) => setPreviewOpen(visible),
+              afterOpenChange: (visible) => !visible && setPreviewImage(''),
+            }}
+            src={previewImage}
+          />
+        )}
+          
         </Form.Item>
         <Form.Item label="Button">
           <Button htmlType="submit">Upload</Button>
